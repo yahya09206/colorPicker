2084fac62f8a8601e354d94aa58717b289483ddb
Yahya Hussein <yahyahussein91@gmail.com>
2017-01-28 09:17:40 -0800

    reset button refreshes to add new colors to divs

--- script.js
+++ script.js
@@ -5,7 +5,20 @@ var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
-var reset = document.querySelector("#reset");
+var resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", function(){
+	//generate all new colors
+	colors = generateRandomColors(6);
+	//pick a new random color from array
+	pickedColor = pickColor();
+	//change colorDisplay to match picked color
+	colorDisplay.textContent = pickedColor;
+	//change colors of squares
+	for(var i = 0; i < squares.length; i++){
+		squares[i].style.background = colors[i];
+	}
+});
 
 colorDisplay.textContent = pickedColor;
 
