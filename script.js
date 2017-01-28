c7c6f46fc757f422c0bfe6a3b4ff052b8da1f9a0
Yahya Hussein <yahyahussein91@gmail.com>
2017-01-27 21:49:07 -0800

    code to update h1 to match color picked

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ var colors = [
 ]
 
 var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+
+colorDisplay.textContent = pickedColor;
+
 for(var i = 0; i < squares.length; i++){
 	squares[i].style.background = colors[i];
 }
