fdff9630d8e3dd15c4c5112f1b7e3e64d4e244ac
Yahya Hussein <yahyahussein91@gmail.com>
2017-01-27 22:19:14 -0800

    updated text for correct answer

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ for(var i = 0; i < squares.length; i++){
 		var clickedColor = this.style.background;
 		//compare color to picked color
 		if(clickedColor === pickedColor){
-			alert("Correct");
+			messageDisplay.textContent = "Correct";
 		} else {
 			this.style.background = "#232323";
 			messageDisplay.textContent = "Try Again"
