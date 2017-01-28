df2c9b370ae2ffb10bf0dc921e37f79e122c7ebd
Yahya Hussein <yahyahussein91@gmail.com>
2017-01-28 09:20:09 -0800

    reset h1 to match winning color

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ resetButton.addEventListener("click", function(){
 	for(var i = 0; i < squares.length; i++){
 		squares[i].style.background = colors[i];
 	}
+	h1.style.background = "#232323";
 });
 
 colorDisplay.textContent = pickedColor;
