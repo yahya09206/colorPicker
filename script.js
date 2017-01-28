df672c027f0d4eebf5c6cb6fc64a480fc8b10f31
Yahya Hussein <yahyahussein91@gmail.com>
2017-01-27 21:43:05 -0800

    looping through to assign diff colors to squares

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ var colors = [
 	"rgb(0,0,255)",
 	"rgb(255,0,255)"
 ]
+
+var square = document.querySelectorAll(".square");
+for(var i = 0; i < squares.length; i++){
+	square[i].background = colors[i];
+}
