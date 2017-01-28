84dae1eca0bef89f8931b2a668598a8abd195a2a
Yahya Hussein <yahyahussein91@gmail.com>
2017-01-28 09:02:38 -0800

    update h1 to change to picked color

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -19,6 +20,7 @@ for(var i = 0; i < squares.length; i++){
 		if(clickedColor === pickedColor){
 			messageDisplay.textContent = "Correct";
 			changeColor(clickedColor);
+			h1.style.background = clickedColor;
 		} else {
 			this.style.background = "#232323";
 			messageDisplay.textContent = "Try Again"
