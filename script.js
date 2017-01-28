f2216df576552a878d910a0a4b71db7237e547ea
Yahya Hussein <yahyahussein91@gmail.com>
2017-01-28 08:46:35 -0800

    function for generating random colors

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 0, 255)"
-]
+var colors = generateRandomColors();
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
