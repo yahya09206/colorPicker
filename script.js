95036de301e79b3b61714b2eb039195947204e66
Yahya Hussein <yahyahussein91@gmail.com>
2017-01-29 22:08:03 -0800

    created variable to store num of colors

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 var colors = generateRandomColors(6);
-
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
@@ -8,6 +7,7 @@ var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
 var easyBtn = document.querySelector("#easyBtn");
 var hardBtn = document.querySelector("#hardBtn");
+var numberOfSquares = 6;
 
 easyBtn.addEventListener("click", function() {
 	hardBtn.classList.remove("selected");
@@ -19,7 +19,7 @@ easyBtn.addEventListener("click", function() {
 		if(colors[i]){
 			squares[i].style.background = colors[i];
 		}else{
-			squares[i].style.display = none;
+			squares[i].style.display = "none";
 		}
 	}
 });
@@ -33,7 +33,6 @@ hardBtn.addEventListener("click", function() {
 	for(var i = 0; i < squares.length; i++){
 			squares[i].style.background = colors[i];
 			squares[i].style.display = "block";
-		}
 	}
 
 });
