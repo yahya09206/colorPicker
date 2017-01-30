537b609bc1e32ad2374c589449b4863b2de3716b
Yahya Hussein <yahyahussein91@gmail.com>
2017-01-30 09:01:02 -0800

    removed play again? when choosing colors

--- script.js
+++ script.js
@@ -47,6 +47,7 @@ resetButton.addEventListener("click", function(){
 	pickedColor = pickColor();
 	//change colorDisplay to match picked color
 	colorDisplay.textContent = pickedColor;
+	this.textContent = "New Colors";
 	messageDisplay.textContent = "";
 	//change colors of squares
 	for(var i = 0; i < squares.length; i++){
