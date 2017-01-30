fa6199c10309d5bac4548979a3be2b6eb9a94ae6
Yahya Hussein <yahyahussein91@gmail.com>
2017-01-30 09:24:03 -0800

    deleted extra code and whitespace

--- script.js
+++ script.js
@@ -40,37 +40,6 @@ function reset(){
 
 }
 
-
-// easyBtn.addEventListener("click", function() {
-// 	hardBtn.classList.remove("selected");
-// 	easyBtn.classList.add("selected");
-// 	numSquares = 3;
-// 	colors = generateRandomColors(numSquares);
-// 	pickedColor = pickColor();
-// 	colorDisplay.textContent = pickedColor;
-// 	for(var i = 0; i < squares.length; i++){
-// 		if(colors[i]){
-// 			squares[i].style.background = colors[i];
-// 		}else{
-// 			squares[i].style.display = "none";
-// 		}
-// 	}
-// });
-
-// hardBtn.addEventListener("click", function() {
-// 	easyBtn.classList.remove("selected");
-// 	hardBtn.classList.add("selected");
-// 	numSquares = 6;
-// 	colors = generateRandomColors(numSquares);
-// 	pickedColor = pickColor();
-// 	colorDisplay.textContent = pickedColor;
-// 	for(var i = 0; i < squares.length; i++){
-// 			squares[i].style.background = colors[i];
-// 			squares[i].style.display = "block";
-// 	}
-
-// });
-
 resetButton.addEventListener("click", function(){
 	reset();
 });
