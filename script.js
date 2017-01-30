a0c020be1580b8fa86633ef95d10006387287431
Yahya Hussein <yahyahussein91@gmail.com>
2017-01-30 09:05:59 -0800

    code for removing class when not clicked on

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ var modeButtons = document.querySelectorAll(".mode");
 
 for(var i = 0; i < modeButtons.length; i++){
 	modeButtons[i].addEventListener("click" function() {
+		modeButtons[0].classList.remove("selected");
+		modeButtons[1].classList.remove("selected");
 		this.classList.add("selected");
 	});
 }
