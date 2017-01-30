c7ba52f087eb442c1224337f2c420d98e53da4c0
Yahya Hussein <yahyahussein91@gmail.com>
2017-01-30 09:03:43 -0800

    selected buttons by new class

--- script.js
+++ script.js
@@ -6,8 +6,11 @@ var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
-var easyBtn = document.querySelector("#easyBtn");
-var hardBtn = document.querySelector("#hardBtn");
+var modeButtons = document.querySelectorAll(".mode");
+
+for(var i = 0; i < modeButtons.length; i++){
+	
+}
 
 
 easyBtn.addEventListener("click", function() {
